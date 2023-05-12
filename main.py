aa341fcdd9d0e68f9598a6ad42f2a6fa0091d607
devUserContact <developer.user.contact@protonmail.com>
2023-05-11 21:08:14 -0700

    readability improvments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
+import os.path as path
 import shutil
-import pandas
-from pandas.core.arrays.string_ import pandas_dtype
+import pandas as pd
 import tiktoken
 from bs4 import BeautifulSoup
 
-path_doc = "./doc_bevy/"
+path_doc = "./rust_doc/"
 path_out = "./out/"
 
 enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -18,17 +18,15 @@ for root, dirs, files in os.walk(path_doc):
             prefix = prefix.replace("/", ",")
             file_no_ext = file.replace(".html", "")
 
-            file_org = str(os.path.join(root, file))
+            file_org = str(path.join(root, file))
             file_new = str(path_out + prefix + "," + file_no_ext)
-
             shutil.copyfile(file_org, file_new)
 
 for root, dirs, files in os.walk(path_out):
     for file in files:
-        tags_array = file.split(",")
-        tags = " ".join(tags_array)
+        tags = " ".join(file.split(","))
 
-        with open(os.path.join(root, file)) as f, open("article.txt", "a") as a:
+        with open(path.join(root, file)) as f, open("article.txt", "a") as a:
             content = f.read().splitlines()
 
             for line in content:
@@ -39,13 +37,13 @@ for root, dirs, files in os.walk(path_out):
 
         article = open("article.txt", "r")
         tokens = len(enc.encode(article.read()))
-
-        article = open("article.txt", "r")
+        article.seek(0)
 
         doc_data["tags"].append(tags)
         doc_data["article"].append(article.read())
         doc_data["tokens"].append(tokens)
+        article.close()
         open("article.txt", "w").close()
 
-df = pandas.DataFrame(doc_data)
+df = pd.DataFrame(doc_data)
 df.to_csv("documentation.csv", index=False, sep=',')
